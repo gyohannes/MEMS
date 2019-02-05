cbc873975f3d89a72462ca94290bdb40509bae8b
G/yohannes Zenebe <gyohannes1@gmail.com>
2019-02-05 15:46:53 +0300

    updating calendar view

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -10,7 +10,7 @@ $(function () {
         header: {
             left: 'prev,next today',
             center: 'title',
-            right: 'month,agendaWeek,agendaDay,listWeek'
+            right: 'month,agendaWeek,agendaDay,listMonth'
         },
         navLinks: true, // can click day/week names to navigate views
         editable: true,
