f0c4af6d8f0cdd42dcc5b260ac23389f5a54acfa
G/yohannes Zenebe <gyohannes1@gmail.com>
2019-12-11 11:46:51 +0300

    update

--- db/migrate/20190128132948_create_news.rb
+++ db/migrate/20190128132948_create_news.rb
@@ -3,7 +3,6 @@ class CreateNews < ActiveRecord::Migration[5.1]
     create_table :news, id: :uuid do |t|
       t.references :user, type: :uuid, foreign_key: true
       t.references :organization_unit, type: :uuid, foreign_key: true
-      t.references :facility, type: :uuid, foreign_key: true
       t.references :institution, type: :uuid, foreign_key: true
       t.string :headline
       t.text :content
