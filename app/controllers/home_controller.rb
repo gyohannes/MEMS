414627cd44f29f005799bfe41c2a5718522ecb64
G/yohannes Zenebe <gyohannes1@gmail.com>
2020-10-17 12:55:31 +0300

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < BaseController
 
   def index
     @deps = Department.joins(:equipment).where('equipment.organization_unit_id = ?', current_user.organization_unit_id).uniq
-    @equipment = current_user.load_equipment
+    @equipment = current_user.is_role(Constants::DEPARTMENT) ? current_user.load_equipment : []
   end
 end
