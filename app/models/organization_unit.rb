560e4ce685343acd636c872cef0b945b8e1cc5c6
G/yohannes Zenebe <gyohannes1@gmail.com>
2021-02-09 16:49:14 +0300

    update

--- app/models/organization_unit.rb
+++ app/models/organization_unit.rb
@@ -119,7 +119,7 @@ class OrganizationUnit < ApplicationRecord
   end
 
   def sub_equipment
-    Equipment.where('organization_unit_id in (?)', (sub_units.pluck(:id) << id))
+    Equipment.includes(:status).where('organization_unit_id in (?)', (sub_units.pluck(:id) << id))
   end
 
   def sub_receive
